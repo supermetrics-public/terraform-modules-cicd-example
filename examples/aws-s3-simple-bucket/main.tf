12ba434c20c85bd89ba7600ca01dbc32b16cc00d
Duy Nguyen <duy.nguyen@supermetrics.com>
2021-04-22 02:02:09 +0300

    Add example aws module + basic terraform fmt check (#1)
    * Add example aws module + basic terraform fmt check
    
    * Fix ci pipeline
    
    * Fix TF fmt
    
    * Update CI pipeline
    
    * Fail the test
    
    * Pass the tests

---
+++ examples/aws-s3-simple-bucket/main.tf
@@ -0,0 +1,69 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "sm-s3-bucket-data" {
+  bucket = "${var.profile}-${var.server_cluster}-${var.environment}-${var.bucket_postfix}"
+  acl    = var.bucket_acl
+
+  force_destroy = var.force_destroy
+
+  # This is a bit horrid
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rule
+    content {
+      id                                     = lookup(lifecycle_rule.value, "id", null)
+      prefix                                 = lookup(lifecycle_rule.value, "prefix", null)
+      tags                                   = lookup(lifecycle_rule.value, "tags", null)
+      abort_incomplete_multipart_upload_days = lookup(lifecycle_rule.value, "abort_incomplete_multipart_upload_days", null)
+      enabled                                = lifecycle_rule.value.enabled
+
+      dynamic "expiration" {
+        for_each = length(keys(lookup(lifecycle_rule.value, "expiration", {}))) == 0 ? [] : [lookup(lifecycle_rule.value, "expiration", {})]
+
+        content {
+          days                         = lookup(expiration.value, "days", null)
+          expired_object_delete_marker = lookup(expiration.value, "expired_object_delete_marker", null)
+        }
+      }
+
+      # Several blocks - transition
+      dynamic "transition" {
+        for_each = lookup(lifecycle_rule.value, "transition", [])
+
+        content {
+          date          = lookup(transition.value, "date", null)
+          days          = lookup(transition.value, "days", null)
+          storage_class = transition.value.storage_class
+        }
+      }
+
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  tags = {
+    AccountId     = data.aws_caller_identity.current.account_id
+    Environment   = var.environment
+    Profile       = var.profile
+    ServerCluster = var.server_cluster
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "sm-s3-bucket-data-public-access" {
+  bucket = aws_s3_bucket.sm-s3-bucket-data.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
---
+++ scripts/helper.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import argparse
+import subprocess
+
+from typing import List
+
+
+def print_usage():
+    print('Run with --help to get more info')
+
+
+def get_working_directories(file_list: str) -> List[str]:
+    files = file_list.split(' ')
+
+    directories = list(set([f.rsplit('/', 1)[0] for f in files if f.endswith('.tf')]))
+    directories.sort()
+
+    return directories
+
+
+def run_terraform_fmt(file_list: str):
+    directories = get_working_directories(file_list)
+
+    is_failure = False
+    for directory in directories:
+        result = subprocess.run(['terraform fmt -check -no-color %s' % directory], capture_output=True, timeout=30, shell=True, universal_newlines=True)
+        
+        if result.returncode != 0:
+            print('Terraform Format error : terraform fmt %s %s' % (result.stdout, result.stderr))
+            is_failure = True
+
+    if is_failure:
+        exit(1)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Helper script')
+
+    subparsers = parser.add_subparsers(dest='func')
+
+    get_working_directories_parser = subparsers.add_parser('get-working-directories', help='Convert list of files to working directories')
+    get_working_directories_parser.add_argument('-f', '--file_list', help='List of changed files separated by space')
+
+    run_terraform_fmt_parser = subparsers.add_parser('run-terraform-fmt', help='Run terraform fmt check on list of working directories')
+    run_terraform_fmt_parser.add_argument('-f', '--file_list', help='List of changed files separated by space')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    if args.func == 'get-working-directories':
+        get_working_directories(args.file_list)
+    elif args.func == 'run-terraform-fmt':
+        run_terraform_fmt(args.file_list)
+    else:
+        print_usage()
