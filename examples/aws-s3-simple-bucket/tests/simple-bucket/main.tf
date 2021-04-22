5b63c35810ba105c916f6954c101b45cf936f503
Duy Nguyen <duy.nguyen@supermetrics.com>
2021-04-22 16:04:49 +0300

    Add TF validate and TF plan into Ci checks (#2)
    * Add TF validate and TF plan into Ci checks
    
    * Configure AWS credentials when running tf plan
    
    * Fix CI
    
    * Add GCP credentials and GCP example module
    
    * Add more log into python script

---
+++ examples/aws-s3-simple-bucket/tests/simple-bucket/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+locals {
+  profile        = "test"
+  environment    = "test"
+  server_cluster = "central"
+  region         = "eu-west-1"
+}
+
+provider "aws" {
+  profile = local.profile
+  region  = local.region
+}
+
+resource "random_string" "this" {
+  length  = 6
+  special = false
+}
+
+module "test_simple_bucket" {
+  source = "../../"
+
+  bucket_postfix = "test-aws-simple-bucket-${random_string.this.result}"
+
+  environment    = local.environment
+  profile        = local.profile
+  server_cluster = local.server_cluster
+}
---
+++ examples/gcp-gcs-simple-bucket/tests/simple-bucket/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+locals {
+  project_id     = "supermetrics-dev-sandbox"
+  environment    = "test"
+  server_cluster = "central"
+  region         = "europe-west1"
+}
+
+provider "google" {
+  project = local.project_id
+  region  = local.region
+}
+
+resource "random_string" "this" {
+  length  = 6
+  special = false
+}
+
+module "bucket_sm_local_backup" {
+  source = "../../"
+
+  environment    = local.environment
+  server_cluster = local.server_cluster
+
+  bucket_name     = "test-gcp-simple-bucket-${random_string.this.result}"
+  project_id      = local.project_id
+  bucket_location = local.region
+
+  storage_class = "STANDARD"
+}
